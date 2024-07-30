85b91c8361dcede8f729abe1f192805de114bd53
Laurian Van Maldeghem <laurian.van.maldeghem@vliz.be>
2024-07-30 13:31:06 +0200

    update repo owner&name extraction (#2)

--- repoRead.py
+++ repoRead.py
@@ -1,30 +1,24 @@
 import requests
-import yaml
-import os
+from urllib.parse import urlparse
 
 from codemeta import createCodemeta
 from githubAPI import gitHubInfo
 
 #Get open-code-repositories
-response = requests.get("https://api.github.com/repos/vliz-be-opsci/open-code-list/contents")
+response = requests.get("https://raw.githubusercontent.com/vliz-be-opsci/open-code-list/main/open-code-list.txt")
 response.raise_for_status() # Raise an exception for HTTP errors
 
-for item in response.json():
-    download_url = item.get("download_url")
-    if download_url:
-        response = requests.get(download_url)
-        response.raise_for_status()
-        open_code_repos = yaml.safe_load(response.text) # Load the YAML content
+urls = response.text.splitlines()
+for url in urls:
+    path = urlparse(url).path.strip('/')
+    path_parts = path.split('/')
 
-
-for repo in open_code_repos:
-    repo_name = repo['name']
-    repo_owner = repo['owner']
-
-    # check if path exists & create in case it doesn't
-    if not os.path.exists(f'./output/{repo_owner}/{repo_name}'):
-        os.makedirs(f'./output/{repo_owner}/{repo_name}')
-
+    if len(path_parts) >= 2:
+        repo_owner = path_parts[0]
+        repo_name = path_parts[1]
+    else:
+        raise ValueError("Invalid GitHub URL format")
+    
     # get information via GitHub API
     gitHubInfo(repo_owner, repo_name)
     # create codemeta file
